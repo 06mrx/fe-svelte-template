41eb105372856f37baf60b308ba8f961c56469ed
06mrx <0611mrx@gmail.com>
2025-09-09 19:55:38 +0800

    fix: fix build spa config

--- svelte.config.js
+++ svelte.config.js
@@ -2,28 +2,23 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		// adapter: adapter()
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-		prerender: {
-			handleUnseenRoutes: 'ignore',
-			handleMissingId: 'ignore'
-			
-		}
-
-
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html', // penting! SPA fallback
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: '',
+    //   assets: '/'  // absolute path untuk _app
+    },
+    prerender: {
+      handleUnseenRoutes: 'ignore',
+      handleMissingId: 'ignore'
+    }
+  }
 };
 
 export default config;
